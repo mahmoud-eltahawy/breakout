d162fd46dcbb1d266e7e2b55d87ccb7f7a2c285e
mahmoud-eltahawy <gemmy9300@gmail.com>
2026-06-22 12:29:58 +0300

    draw walls

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::{
+    color::palettes::css::GREEN,
     prelude::*,
     window::{PrimaryWindow, WindowLevel},
 };
@@ -7,15 +8,16 @@ const WINDOW_WIDTH: u32 = 800;
 const WINDOW_HEIGHT: u32 = 600;
 const WINDOW_MARGIN: f32 = 20.;
 
-const RECT_WIDTH: f32 = 40.;
-const RECT_HEIGHT: f32 = 20.;
+const RECT_SIZE: Vec2 = Vec2::new(40., 20.);
 const RECT_GAP: f32 = 10.;
 
-const BAR_WIDTH: f32 = 140.;
-const BAR_HEIGHT: f32 = 10.;
+const BAR_SIZE: Vec2 = Vec2::new(140., 10.);
 
 const RECTS_COLUMNS: usize = 14;
 const RECTS_ROWS: usize = 9;
+
+const BALL_SIZE: f32 = 9.;
+const BALL_INIT_POSITION: Vec2 = Vec2::new(0., 0.);
 
 fn main() {
     App::new()
@@ -31,8 +33,8 @@ fn main() {
             ..Default::default()
         }))
         .insert_resource(ClearColor(Color::srgb(0.0, 0.0, 0.0)))
-        .add_systems(Startup, (setup, setup_rects, setup_bar))
-        .add_systems(Update, bar_position)
+        .add_systems(Startup, (setup, setup_rects, setup_bar, setup_ball))
+        .add_systems(Update, (bar_controller, setup_walls))
         .run();
 }
 
@@ -48,9 +50,9 @@ fn setup_rects(
     mut meshes: ResMut<Assets<Mesh>>,
     mut matriels: ResMut<Assets<ColorMaterial>>,
 ) {
-    let rect_mesh = meshes.add(Rectangle::new(RECT_WIDTH, RECT_HEIGHT));
+    let rect_mesh = meshes.add(Rectangle::new(RECT_SIZE.x, RECT_SIZE.y));
     let rect_color = Color::srgba(1., 1., 1., 1.);
-    let rect_y = WINDOW_HEIGHT as f32 / 2. - RECT_HEIGHT / 2. - WINDOW_MARGIN;
+    let rect_y = WINDOW_HEIGHT as f32 / 2. - RECT_SIZE.y / 2. - WINDOW_MARGIN;
 
     for i in 1..=(RECTS_COLUMNS / 2) {
         for j in 0..RECTS_ROWS {
@@ -58,8 +60,8 @@ fn setup_rects(
                 Mesh2d(rect_mesh.clone()),
                 MeshMaterial2d(matriels.add(rect_color)),
                 Transform::from_xyz(
-                    (RECT_WIDTH + RECT_GAP) * i as f32,
-                    rect_y - (RECT_HEIGHT + RECT_GAP) * j as f32,
+                    (RECT_SIZE.x + RECT_GAP) * i as f32,
+                    rect_y - (RECT_SIZE.y + RECT_GAP) * j as f32,
                     0.,
                 ),
                 Rect,
@@ -68,8 +70,8 @@ fn setup_rects(
                 Mesh2d(rect_mesh.clone()),
                 MeshMaterial2d(matriels.add(rect_color)),
                 Transform::from_xyz(
-                    -(RECT_WIDTH + RECT_GAP) * i as f32,
-                    rect_y - (RECT_HEIGHT + RECT_GAP) * j as f32,
+                    -(RECT_SIZE.x + RECT_GAP) * i as f32,
+                    rect_y - (RECT_SIZE.y + RECT_GAP) * j as f32,
                     0.,
                 ),
                 Rect,
@@ -86,29 +88,68 @@ fn setup_bar(
     mut meshes: ResMut<Assets<Mesh>>,
     mut matriels: ResMut<Assets<ColorMaterial>>,
 ) {
-    let rect_mesh = meshes.add(Rectangle::new(BAR_WIDTH, BAR_HEIGHT));
+    let rect_mesh = meshes.add(Rectangle::new(BAR_SIZE.x, BAR_SIZE.y));
     let rect_color = Color::srgba(1., 1., 1., 1.);
 
     commands.spawn((
         Mesh2d(rect_mesh.clone()),
         MeshMaterial2d(matriels.add(rect_color)),
-        Transform::from_xyz(
-            0.,
-            -(WINDOW_HEIGHT as f32) / 2. + WINDOW_MARGIN + RECT_GAP,
-            0.,
-        ),
+        Transform::from_xyz(0., -(WINDOW_HEIGHT as f32) / 2. + WINDOW_MARGIN, 0.),
         Bar,
     ));
 }
 
-fn bar_position(
+fn bar_controller(
     window: Single<&Window, With<PrimaryWindow>>,
     mut bar: Single<&mut Transform, With<Bar>>,
 ) {
     if let Some(position) = window.cursor_position() {
         const HWW: f32 = (WINDOW_WIDTH / 2) as f32;
-        const HBW: f32 = BAR_WIDTH / 2.;
-        let x = position.x - HWW as f32;
+        const HBW: f32 = BAR_SIZE.x / 2.;
+        let x = position.x - HWW;
         bar.translation.x = x.clamp(-HWW + HBW, HWW - HBW);
+    }
+}
+
+#[derive(Component)]
+struct Ball;
+
+fn setup_ball(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut matriels: ResMut<Assets<ColorMaterial>>,
+) {
+    let ball_mesh = meshes.add(Circle::new(BALL_SIZE));
+    let ball_color = Color::srgba(0., 1., 1., 1.);
+
+    commands.spawn((
+        Mesh2d(ball_mesh.clone()),
+        MeshMaterial2d(matriels.add(ball_color)),
+        Transform::from_xyz(BALL_INIT_POSITION.x, BALL_INIT_POSITION.y, 0.),
+        Ball,
+    ));
+}
+
+const BOTTOM_LEFT: Vec2 = Vec2 {
+    x: -((WINDOW_WIDTH / 2) as f32),
+    y: -(WINDOW_HEIGHT as f32) / 2.,
+};
+const TOP_LEFT: Vec2 = Vec2 {
+    x: -((WINDOW_WIDTH / 2) as f32),
+    y: (WINDOW_HEIGHT as f32) / 2.,
+};
+const TOP_RIGHT: Vec2 = Vec2 {
+    x: (WINDOW_WIDTH / 2) as f32,
+    y: (WINDOW_HEIGHT as f32) / 2.,
+};
+const BOTTOM_RIGHT: Vec2 = Vec2 {
+    x: ((WINDOW_WIDTH / 2) as f32),
+    y: -(WINDOW_HEIGHT as f32) / 2.,
+};
+const CORNERS: [Vec2; 4] = [BOTTOM_LEFT, TOP_LEFT, TOP_RIGHT, BOTTOM_RIGHT];
+
+fn setup_walls(mut gizmos: Gizmos) {
+    for (begin, end) in CORNERS.windows(2).map(|xs| (xs[0], xs[1])) {
+        gizmos.line_2d(begin, end, GREEN);
     }
 }
